fb21dafb73c611ad58e9dd75e31262b7a0d5442c
AbrahamRamosM <abraham.ramos.morales@uabc.edu.mx>
2022-09-24 02:49:38 -0700

    Options UI changes

--- src/components/SpotifyPlayer/SpotifyComponents/RefreshButton.tsx
+++ src/components/SpotifyPlayer/SpotifyComponents/RefreshButton.tsx
@@ -1,6 +1,8 @@
 const RefreshButton = ({ handleClick }: any) => {
   return (
     <>
+      <div className="flex h-7 rounded-xl w-[1px] bg-spotify-gray/20 items-center justify-center align-middle" />
+
       <button onClick={handleClick} title="Refresh">
         <svg
           className="w-6 h-6 duration-150 transition-all ease-in block hover:text-spotify-green text-spotify-gray"
@@ -10,7 +12,7 @@ const RefreshButton = ({ handleClick }: any) => {
           <path fill="none" d="M0 0h24v24H0z" />
           <path
             fill="currentColor"
-            d="M5.463 4.433A9.961 9.961 0 0 1 12 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228l-.997-1.795zm13.074 15.134A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772l.997 1.795z"
+            d="M18.537 19.567A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10c0 2.136-.67 4.116-1.81 5.74L17 12h3a8 8 0 1 0-2.46 5.772l.997 1.795z"
           />
         </svg>
       </button>
