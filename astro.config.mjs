b1557901bcd6f316cef98be99686c12ad8617d5f
AbrahamRamosM <abraham.ramos.morales@uabc.edu.mx>
2022-09-24 17:37:14 -0700

    Astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), svelte()],
+  output: "server",
+  adapter: vercel(),
   site: "https://abraham-dev.vercel.app",
 });
