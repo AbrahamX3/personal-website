7615aea8a5de2fa6a09ae1c308378bcde1bad75a
AbrahamX3 <abrahamramos.info@gmail.com>
2022-12-26 06:49:28 -0800

    <a> to span

--- src/components/SpotifyPlayer/SpotifyOptions.tsx
+++ src/components/SpotifyPlayer/SpotifyOptions.tsx
@@ -9,7 +9,7 @@ const Options = ({ repeat_state, shuffle_state }: OptionsProps) => {
   return (
     <>
       {shuffle_state ? (
-        <a title="Shuffle Enabled">
+        <span title="Shuffle Enabled">
           <svg
             className="w-6 h-8 fill-spotify-green"
             xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +18,7 @@ const Options = ({ repeat_state, shuffle_state }: OptionsProps) => {
             <path fill="none" d="M0 0h24v24H0z" />
             <path d="M18 17.883V16l5 3-5 3v-2.09a9 9 0 0 1-6.997-5.365L11 14.54l-.003.006A9 9 0 0 1 2.725 20H2v-2h.725a7 7 0 0 0 6.434-4.243L9.912 12l-.753-1.757A7 7 0 0 0 2.725 6H2V4h.725a9 9 0 0 1 8.272 5.455L11 9.46l.003-.006A9 9 0 0 1 18 4.09V2l5 3-5 3V6.117a7 7 0 0 0-5.159 4.126L12.088 12l.753 1.757A7 7 0 0 0 18 17.883z" />
           </svg>
-        </a>
+        </span>
       ) : (
         <a title="Shuffle Disabled">
           <svg
@@ -33,7 +33,7 @@ const Options = ({ repeat_state, shuffle_state }: OptionsProps) => {
       )}
 
       {repeat_state == "off" ? (
-        <a title="Repeat Disabled">
+        <span title="Repeat Disabled">
           <svg
             className="w-6 h-8 fill-spotify-gray"
             xmlns="http://www.w3.org/2000/svg"
@@ -42,9 +42,9 @@ const Options = ({ repeat_state, shuffle_state }: OptionsProps) => {
             <path fill="none" d="M0 0h24v24H0z" />
             <path d="M8 20v1.932a.5.5 0 0 1-.82.385l-4.12-3.433A.5.5 0 0 1 3.382 18H18a2 2 0 0 0 2-2V8h2v8a4 4 0 0 1-4 4H8zm8-16V2.068a.5.5 0 0 1 .82-.385l4.12 3.433a.5.5 0 0 1-.321.884H6a2 2 0 0 0-2 2v8H2V8a4 4 0 0 1 4-4h10z" />
           </svg>
-        </a>
+        </span>
       ) : repeat_state == "track" ? (
-        <a title="Repeat One Enabled">
+        <span title="Repeat One Enabled">
           <svg
             className="w-6 h-8 text-spotify-green"
             xmlns="http://www.w3.org/2000/svg"
@@ -56,9 +56,9 @@ const Options = ({ repeat_state, shuffle_state }: OptionsProps) => {
               d="M8 20v1.932a.5.5 0 0 1-.82.385l-4.12-3.433A.5.5 0 0 1 3.382 18H18a2 2 0 0 0 2-2V8h2v8a4 4 0 0 1-4 4H8zm8-16V2.068a.5.5 0 0 1 .82-.385l4.12 3.433a.5.5 0 0 1-.321.884H6a2 2 0 0 0-2 2v8H2V8a4 4 0 0 1 4-4h10zm-5 4h2v8h-2v-6H9V9l2-1z"
             />
           </svg>
-        </a>
+        </span>
       ) : (
-        <a title="Repeat Enabled">
+        <span title="Repeat Enabled">
           <svg
             className="w-6 h-8 text-spotify-green"
             xmlns="http://www.w3.org/2000/svg"
@@ -70,7 +70,7 @@ const Options = ({ repeat_state, shuffle_state }: OptionsProps) => {
               d="M8 20v1.932a.5.5 0 0 1-.82.385l-4.12-3.433A.5.5 0 0 1 3.382 18H18a2 2 0 0 0 2-2V8h2v8a4 4 0 0 1-4 4H8zm8-16V2.068a.5.5 0 0 1 .82-.385l4.12 3.433a.5.5 0 0 1-.321.884H6a2 2 0 0 0-2 2v8H2V8a4 4 0 0 1 4-4h10z"
             />
           </svg>
-        </a>
+        </span>
       )}
     </>
   );
