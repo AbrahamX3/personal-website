d2da92c1715dbb7cb331e408279e0029c26aa5fa
AbrahamX3 <abrahamramos.info@gmail.com>
2023-02-25 16:36:05 -0800

    fixed urls on socials

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function Socials() {
   const Socials = [
     {
@@ -19,7 +17,7 @@ export default function Socials() {
   return (
     <div className="mx-auto grid grid-cols-1 justify-center justify-items-center gap-4 xs:grid-cols-3">
       {Socials.map((social, index) => (
-        <Link
+        <a
           key={index}
           title={social.name}
           href={social.url}
@@ -29,7 +27,7 @@ export default function Socials() {
           <span className="rounded-lg border-2 border-white/10 bg-white/10 px-2 py-1 text-center text-white backdrop-blur-md transition-colors duration-150 ease-in hover:border-white/30">
             {social.name}
           </span>
-        </Link>
+        </a>
       ))}
     </div>
   );
